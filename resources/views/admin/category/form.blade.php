a1cc4dcf01a6ec7939941c291a4d834cea5a1d4d
mehribanaskerova <74506433+mehribanaskerova@users.noreply.github.com>
2023-06-01 23:57:11 +0500

    language input

--- resources/views/admin/category/form.blade.php
+++ resources/views/admin/category/form.blade.php
@@ -42,32 +42,6 @@
                                         <span class="text-danger">{{$message}}</span>
                                         @enderror
                                     </div>
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card card-primary card-tabs">
-                    <div class="card-header p-0 pt-1">
-                        <ul class="nav nav-tabs" id="custom-tabs-two-tab" role="tablist">
-                            <li class="pt-2 px-3"><h3 class="card-title">Slug</h3></li>
-                            @foreach(config('app.languages') as $langKey)
-                                <li class="nav-item ">
-                                    <a class="nav-link {{$loop->first ? ' active ' : '' }}
-                                          @error("slug.$langKey") text-danger @enderror"
-                                       id="custom-tabs-two-home-tab" data-toggle="pill" href="#slug-{{$langKey}}"
-                                       role="tab" aria-controls="custom-tabs-two-home" aria-selected="true">
-                                        {{\Illuminate\Support\Str::upper($langKey)}}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    <div class="card-body">
-                        <div class="tab-content" id="custom-tabs-one-tabContent">
-                            @foreach(config('app.languages') as $lang)
-                                <div class="tab-pane fade {{$loop->first ? ' active show' : '' }}" id="slug-{{$lang}}"
-                                     role="tabpanel" aria-labelledby="custom-tabs-two-home-tab">
                                     <div class="form-group">
                                         <label>Slug {{$lang}}</label>
                                         <input type="text" placeholder="Slug {{$lang}}" name="{{$lang}}[slug]"
@@ -82,6 +56,41 @@
                         </div>
                     </div>
                 </div>
+
+{{--                <div class="card card-primary card-tabs">--}}
+{{--                    <div class="card-header p-0 pt-1">--}}
+{{--                        <ul class="nav nav-tabs" id="custom-tabs-two-tab" role="tablist">--}}
+{{--                            <li class="pt-2 px-3"><h3 class="card-title">Slug</h3></li>--}}
+{{--                            @foreach(config('app.languages') as $langKey)--}}
+{{--                                <li class="nav-item ">--}}
+{{--                                    <a class="nav-link {{$loop->first ? ' active ' : '' }}--}}
+{{--                                          @error("slug.$langKey") text-danger @enderror"--}}
+{{--                                       id="custom-tabs-two-home-tab" data-toggle="pill" href="#slug-{{$langKey}}"--}}
+{{--                                       role="tab" aria-controls="custom-tabs-two-home" aria-selected="true">--}}
+{{--                                        {{\Illuminate\Support\Str::upper($langKey)}}</a>--}}
+{{--                                </li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                    <div class="card-body">--}}
+{{--                        <div class="tab-content" id="custom-tabs-one-tabContent">--}}
+{{--                            @foreach(config('app.languages') as $lang)--}}
+{{--                                <div class="tab-pane fade {{$loop->first ? ' active show' : '' }}" id="slug-{{$lang}}"--}}
+{{--                                     role="tabpanel" aria-labelledby="custom-tabs-two-home-tab">--}}
+{{--                                    <div class="form-group">--}}
+{{--                                        <label>Slug {{$lang}}</label>--}}
+{{--                                        <input type="text" placeholder="Slug {{$lang}}" name="{{$lang}}[slug]"--}}
+{{--                                               value="{{old("$lang.slug",isset($model) ? ($model->translateOrDefault($lang)->slug ?? '') : '')}}"--}}
+{{--                                               class="form-control">--}}
+{{--                                        @error("$lang.slug")--}}
+{{--                                        <span class="text-danger">{{$message}}</span>--}}
+{{--                                        @enderror--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            @endforeach--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
                 <div class="form-group">
                     <label>Parent Category</label>
